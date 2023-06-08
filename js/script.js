7383160fa3b94aad3fdb384ca4305a2be4ce06c9
Saurabh Panchal <saurabh.panchal1018@gmail.com>
2023-06-08 16:51:41 +0530

    Minor Fixes and Added README.MD

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 ///////////////////////////////////////////////////////////
 // Set current year
 const yearEl = document.querySelector('.year');
-console.log(yearEl);
-console.log(yearEl.textContent);
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
